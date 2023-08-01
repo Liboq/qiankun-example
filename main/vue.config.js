e0683b5a467aa478383746cdd8f21a994c661249
Liboq <2661158759@qq.com>
2023-08-01 11:21:45 +0800

    部署上线

--- main/vue.config.js
+++ main/vue.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   transpileDependencies: ['common'],
+  publicPath: '/main',
   chainWebpack: config => {
     config.plugin('html')
       .tap((args) => {
         args[0].title = 'qiankun-example'
         return args
       })
-  }
+  },
 }
